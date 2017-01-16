6ebe9bbe10e6f07a1a44778038f3388bc28b947f
阿东 <konaod@gmail.com>
2017-01-16 18:06:49 +0800

    丰富日志打印的信息

--- xjf-ios-mvvm/Helpers/Logger/Logger.swift
+++ xjf-ios-mvvm/Helpers/Logger/Logger.swift
@@ -10,40 +10,55 @@ import Foundation
 
 class Logger: NSObject {
 
-  fileprivate static let tag = "Localizable"
-
-#if DEBUG
+    #if DEBUG
     static var logLevel = LogLevel.Info
-#else
+    #else
     static var logLevel = LogLevel.Warning
-#endif
+    #endif
 
-  class func logHttp(message: String) {
-    log(message: message, logLevel: .Http)
-  }
+    static var dateFormatter: DateFormatter {
+        struct Statics {
+            static var dateFormatter: DateFormatter = {
+                let defaultDateFormatter = DateFormatter()
+                defaultDateFormatter.locale = NSLocale.current
+                defaultDateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+                return defaultDateFormatter
+            }()
+        }
+        return Statics.dateFormatter
+    }
 
-  class func logError(message: String) {
-    log(message: message, logLevel: .Error)
-  }
+    class func logHttp(message: String, functionName: StaticString = #function, fileName: StaticString = #file, lineNumber: Int = #line) {
+        log(message: message, logLevel: .Http, functionName: functionName, fileName: fileName, lineNumber: lineNumber)
+    }
 
-  class func logWarning(message: String) {
-    log(message: message, logLevel: .Warning)
-  }
+    class func logError(message: String, functionName: StaticString = #function, fileName: StaticString = #file, lineNumber: Int = #line) {
+        log(message: message, logLevel: .Error, functionName: functionName, fileName: fileName, lineNumber: lineNumber)
+    }
 
-  class func logInfo(message: String) {
-    log(message: message, logLevel: .Info)
-  }
+    class func logWarning(message: String, functionName: StaticString = #function, fileName: StaticString = #file, lineNumber: Int = #line) {
+        log(message: message, logLevel: .Warning, functionName: functionName, fileName: fileName, lineNumber: lineNumber)
+    }
+
+    class func logInfo(message: String, functionName: StaticString = #function, fileName: StaticString = #file, lineNumber: Int = #line) {
+        log(message: message, logLevel: .Info, functionName: functionName, fileName: fileName, lineNumber: lineNumber)
+    }
 
 }
 
 fileprivate extension Logger {
 
-  fileprivate class func log(message: String, logLevel: LogLevel) {
-    if logLevel.rawValue <= Logger.logLevel.rawValue {
-      let messages = message.components(separatedBy: "\n")
-      messages.forEach {
-        print("[\(tag)][\(logLevel.name)]: \($0)")
-      }
+    fileprivate class func log(message: String, logLevel: LogLevel,
+                               functionName: StaticString,
+                               fileName: StaticString, lineNumber: Int) {
+        if logLevel.rawValue <= Logger.logLevel.rawValue {
+            let dateStr = dateFormatter.string(from: Date())
+            let funcName = String(describing: functionName)
+            let filename = (String(describing: fileName) as NSString).lastPathComponent
+            let messages = message.components(separatedBy: "\n")
+            messages.forEach {
+                print("\(dateStr) [\(logLevel.name)] [\(filename):\(lineNumber)] \(funcName) > \($0)")
+            }
+        }
     }
-  }
 }
