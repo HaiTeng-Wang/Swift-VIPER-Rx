124e26432960511d1bd74a1aa3f1fbcdd9b18106
junyi <zjywill@hotmail.com>
2017-02-15 16:15:44 +0800

    增加进度条

--- xjf-ios-mvvm/Modules/Web/View/WebViewController.swift
+++ xjf-ios-mvvm/Modules/Web/View/WebViewController.swift
@@ -16,6 +16,7 @@ class WebViewController: UIViewController {
     var shareEnable: Bool = false
 
     private lazy var webView: WKWebView = self.createWebView()
+    private lazy var progressView: UIProgressView = self.createProgressView()
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -36,12 +37,19 @@ class WebViewController: UIViewController {
 
         view.backgroundColor = UIColor.gray
         view.addSubview(webView)
+        view.addSubview(progressView)
 
         let screenWidth = UIScreen.main.bounds.width
+
+        progressView.snp.makeConstraints { make in
+            make.width.equalTo(screenWidth)
+            make.height.equalTo(3)
+        }
 
         webView.snp.makeConstraints { make in
             make.width.equalTo(screenWidth)
             make.height.equalToSuperview()
+            make.top.equalTo(progressView.snp.top)
         }
 
         let url = URL(string: "https://m.baidu.com")
@@ -63,8 +71,12 @@ class WebViewController: UIViewController {
         if let naviVC = self.navigationController {
             naviVC.setNavigationBarHidden(false, animated: false)
             naviVC.navigationBar.backgroundColor = UIColor.gray
-            self.title = "WEBVIEW"
         }
+    }
+
+    func setNaviTitle(title: String?) {
+        Logger.logInfo(message: "setNaviTitle \(title)")
+        self.title = title
     }
 
     // MARK: WebViewInput
@@ -73,6 +85,35 @@ class WebViewController: UIViewController {
 
     func createWebView() -> WKWebView {
         let view = WKWebView()
+        view.navigationDelegate = self
+        view.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
         return view
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if (keyPath == "estimatedProgress") {
+            progressView.isHidden = webView.estimatedProgress == 1
+            progressView.setProgress(Float(webView.estimatedProgress), animated: true)
+        }
+    }
+
+    func createProgressView() -> UIProgressView {
+        let view = UIProgressView()
+        view.backgroundColor = UIColor.gray
+        view.progressTintColor = UIColor.blue
+        view.trackTintColor = UIColor.gray
+        view.progress = 0.3
+        return view
+    }
+
+    deinit {
+        webView.removeObserver(self, forKeyPath: "estimatedProgress")
+    }
+}
+
+extension WebViewController:WKNavigationDelegate {
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        Logger.logInfo(message: "setNaviTitle A \(title)")
+        self.setNaviTitle(title: webView.title)
     }
 }
